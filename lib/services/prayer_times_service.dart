0e5b4ad2c2cb7b26947fb6b2bccaf4b27bbf4f8f
Adam HJ <aadmrith769@gmail.com>
2025-10-27 15:03:42 +0800

    improve accuracy

--- lib/services/prayer_times_service.dart
+++ lib/services/prayer_times_service.dart
@@ -14,14 +14,8 @@ class PrayerTimesService {
       String zoneCode = _getZoneFromCoordinates(latitude, longitude);
       final result = await getPrayerTimesByZone(zoneCode);
 
-      // Add the location name based on the zone
-      if (result != null &&
-          result['data'] != null &&
-          result['data']['meta'] != null) {
-        result['data']['meta']['locationName'] = _getLocationNameFromZone(
-          zoneCode,
-        );
-      }
+      // Don't override location name here - let the UI handle it separately
+      // This allows for more accurate location names from geocoding
 
       return result;
     } catch (e) {
@@ -56,47 +50,196 @@ class PrayerTimesService {
 
   // Map coordinates to Malaysian prayer zone codes (for e-solat.gov.my API)
   static String _getZoneFromCoordinates(double latitude, double longitude) {
-    // Major Malaysian cities and their zone codes (as used by e-solat.gov.my API)
+    // JAKIM e-solat zones with their representative coordinates
     final zones = {
       // Wilayah Persekutuan
-      'WLY01': {'lat': 3.1390, 'lng': 101.6869},
-
+      'WLY01': {
+        'lat': 3.1390,
+        'lng': 101.6869,
+        'name': 'Kuala Lumpur',
+      }, // KL & surrounding
       // Selangor
-      'SGR01': {'lat': 3.0738, 'lng': 101.5183},
-
+      'SGR01': {
+        'lat': 3.0738,
+        'lng': 101.5183,
+        'name': 'Shah Alam',
+      }, // Shah Alam, Klang
+      'SGR02': {
+        'lat': 2.9264,
+        'lng': 101.6550,
+        'name': 'Putrajaya',
+      }, // Putrajaya, Cyberjaya
+      'SGR03': {
+        'lat': 3.2169,
+        'lng': 101.7285,
+        'name': 'Petaling Jaya',
+      }, // PJ, Subang
       // Johor
-      'JHR01': {'lat': 1.4927, 'lng': 103.7414},
-      'JHR02': {'lat': 2.0581, 'lng': 102.5689},
-
+      'JHR01': {
+        'lat': 1.4927,
+        'lng': 103.7414,
+        'name': 'Johor Bahru',
+      }, // JB North
+      'JHR02': {
+        'lat': 2.0581,
+        'lng': 102.5689,
+        'name': 'Batu Pahat',
+      }, // JB South, Batu Pahat
+      'JHR03': {
+        'lat': 2.2008,
+        'lng': 102.2501,
+        'name': 'Kluang',
+      }, // Kluang, Mersing
+      'JHR04': {
+        'lat': 1.8633,
+        'lng': 103.1408,
+        'name': 'Pontian',
+      }, // Pontian, Tangkak
       // Penang
-      'PNG01': {'lat': 5.4164, 'lng': 100.3327},
-
+      'PNG01': {
+        'lat': 5.4164,
+        'lng': 100.3327,
+        'name': 'Pulau Pinang',
+      }, // Georgetown
       // Perak
-      'PRK01': {'lat': 4.5975, 'lng': 101.0901},
-
+      'PRK01': {
+        'lat': 4.5975,
+        'lng': 101.0901,
+        'name': 'Ipoh',
+      }, // Ipoh, Taiping
+      'PRK02': {
+        'lat': 5.4164,
+        'lng': 100.3327,
+        'name': 'Taiping',
+      }, // Taiping, Larut
+      'PRK03': {
+        'lat': 3.8077,
+        'lng': 101.0901,
+        'name': 'Teluk Intan',
+      }, // Teluk Intan
+      'PRK04': {
+        'lat': 4.1842,
+        'lng': 100.6328,
+        'name': 'Kuala Kangsar',
+      }, // Kuala Kangsar
+      'PRK05': {
+        'lat': 5.3294,
+        'lng': 100.2737,
+        'name': 'Pulau Pinang',
+      }, // Seberang Perai
+      'PRK06': {
+        'lat': 3.4927,
+        'lng': 101.7414,
+        'name': 'Selama',
+      }, // Selama, Taiping
+      'PRK07': {
+        'lat': 4.7698,
+        'lng': 100.9381,
+        'name': 'Bagan Datuk',
+      }, // Bagan Datuk
       // Kedah
-      'KDH01': {'lat': 6.1184, 'lng': 100.3685},
-
+      'KDH01': {
+        'lat': 6.1184,
+        'lng': 100.3685,
+        'name': 'Alor Setar',
+      }, // Alor Setar
+      'KDH02': {
+        'lat': 5.6921,
+        'lng': 100.4917,
+        'name': 'Sungai Petani',
+      }, // Sungai Petani
+      'KDH03': {
+        'lat': 6.4414,
+        'lng': 100.1986,
+        'name': 'Kuala Muda',
+      }, // Kuala Muda
+      'KDH04': {
+        'lat': 5.8947,
+        'lng': 100.3685,
+        'name': 'Kubang Pasu',
+      }, // Kubang Pasu
+      'KDH05': {
+        'lat': 6.1254,
+        'lng': 100.3685,
+        'name': 'Pokok Sena',
+      }, // Pokok Sena
+      'KDH06': {
+        'lat': 5.8077,
+        'lng': 100.5381,
+        'name': 'Padang Terap',
+      }, // Padang Terap
+      'KDH07': {'lat': 6.4414, 'lng': 100.5381, 'name': 'Langkawi'}, // Langkawi
       // Kelantan
-      'KTN01': {'lat': 6.1254, 'lng': 102.2386},
-
+      'KTN01': {
+        'lat': 6.1254,
+        'lng': 102.2386,
+        'name': 'Kota Bharu',
+      }, // Kota Bharu
+      'KTN02': {'lat': 5.8947, 'lng': 102.2386, 'name': 'Tumpat'}, // Tumpat
+      'KTN03': {
+        'lat': 6.4414,
+        'lng': 101.9381,
+        'name': 'Pasir Mas',
+      }, // Pasir Mas
       // Terengganu
-      'TRG01': {'lat': 5.3302, 'lng': 103.1408},
-
+      'TRG01': {
+        'lat': 5.3302,
+        'lng': 103.1408,
+        'name': 'Kuala Terengganu',
+      }, // Kuala Terengganu
+      'TRG02': {'lat': 4.7698, 'lng': 103.1408, 'name': 'Besut'}, // Besut
+      'TRG03': {'lat': 5.8077, 'lng': 102.5689, 'name': 'Setiu'}, // Setiu
+      'TRG04': {'lat': 4.4927, 'lng': 103.3260, 'name': 'Marang'}, // Marang
       // Pahang
-      'PHG01': {'lat': 3.8077, 'lng': 103.3260},
-
+      'PHG01': {'lat': 3.8077, 'lng': 103.3260, 'name': 'Kuantan'}, // Kuantan
+      'PHG02': {
+        'lat': 3.7927,
+        'lng': 101.8574,
+        'name': 'Raub',
+      }, // Raub, Bentong
+      'PHG03': {'lat': 3.9414, 'lng': 102.3685, 'name': 'Jerantut'}, // Jerantut
+      'PHG04': {'lat': 3.4927, 'lng': 102.3685, 'name': 'Temerloh'}, // Temerloh
+      'PHG05': {'lat': 4.1842, 'lng': 101.9381, 'name': 'Rompin'}, // Rompin
+      'PHG06': {'lat': 3.2169, 'lng': 101.7285, 'name': 'Maran'}, // Maran
       // Negeri Sembilan
-      'NSN01': {'lat': 2.7297, 'lng': 101.9381},
-
+      'NSN01': {'lat': 2.7297, 'lng': 101.9381, 'name': 'Seremban'}, // Seremban
+      'NSN02': {
+        'lat': 2.4927,
+        'lng': 102.2501,
+        'name': 'Port Dickson',
+      }, // Port Dickson
+      'NSN03': {'lat': 2.2008, 'lng': 102.2501, 'name': 'Jempol'}, // Jempol
       // Melaka
-      'MLK01': {'lat': 2.1896, 'lng': 102.2501},
-
+      'MLK01': {'lat': 2.1896, 'lng': 102.2501, 'name': 'Melaka'}, // Melaka
       // Sabah
-      'SBH01': {'lat': 5.9804, 'lng': 116.0735},
-
+      'SBH01': {
+        'lat': 5.9804,
+        'lng': 116.0735,
+        'name': 'Kota Kinabalu',
+      }, // Kota Kinabalu
+      'SBH02': {'lat': 5.3294, 'lng': 115.2386, 'name': 'Beaufort'}, // Beaufort
+      'SBH03': {'lat': 4.4927, 'lng': 118.1408, 'name': 'Kudat'}, // Kudat
+      'SBH04': {'lat': 5.8077, 'lng': 116.7381, 'name': 'Sandakan'}, // Sandakan
+      'SBH05': {
+        'lat': 4.7698,
+        'lng': 115.2386,
+        'name': 'Lahad Datu',
+      }, // Lahad Datu
+      'SBH06': {'lat': 5.6921, 'lng': 117.0735, 'name': 'Tawau'}, // Tawau
       // Sarawak
-      'SWK01': {'lat': 1.5533, 'lng': 110.3592},
+      'SWK01': {'lat': 1.5533, 'lng': 110.3592, 'name': 'Kuching'}, // Kuching
+      'SWK02': {
+        'lat': 2.2008,
+        'lng': 111.9381,
+        'name': 'Samarahan',
+      }, // Samarahan
+      'SWK03': {'lat': 3.2169, 'lng': 113.3260, 'name': 'Sri Aman'}, // Sri Aman
+      'SWK04': {'lat': 2.4927, 'lng': 112.9381, 'name': 'Betong'}, // Betong
+      'SWK05': {'lat': 1.8633, 'lng': 111.9381, 'name': 'Sarikei'}, // Sarikei
+      'SWK06': {'lat': 3.8077, 'lng': 114.3685, 'name': 'Kapit'}, // Kapit
+      'SWK07': {'lat': 3.4927, 'lng': 115.7381, 'name': 'Bintulu'}, // Bintulu
+      'SWK08': {'lat': 4.1842, 'lng': 114.9381, 'name': 'Miri'}, // Miri
+      'SWK09': {'lat': 3.9414, 'lng': 113.7381, 'name': 'Limbang'}, // Limbang
     };
 
     String closestZone = 'WLY01'; // Default to Kuala Lumpur
@@ -115,26 +258,95 @@ class PrayerTimesService {
       }
     });
 
+    print(
+      'Selected zone $closestZone (${zones[closestZone]!['name']}) for coordinates ($latitude, $longitude)',
+    );
     return closestZone;
   }
 
   // Get display name from zone code
   static String _getLocationNameFromZone(String zoneCode) {
     final zoneDisplayNames = {
-      'WLY01': 'Wilayah Persekutuan',
-      'SGR01': 'Selangor',
-      'JHR01': 'Johor',
-      'JHR02': 'Johor',
+      // Wilayah Persekutuan
+      'WLY01': 'Kuala Lumpur',
+
+      // Selangor
+      'SGR01': 'Shah Alam',
+      'SGR02': 'Putrajaya',
+      'SGR03': 'Petaling Jaya',
+
+      // Johor
+      'JHR01': 'Johor Bahru',
+      'JHR02': 'Batu Pahat',
+      'JHR03': 'Kluang',
+      'JHR04': 'Pontian',
+
+      // Penang
       'PNG01': 'Pulau Pinang',
-      'PRK01': 'Perak',
-      'KDH01': 'Kedah',
-      'KTN01': 'Kelantan',
-      'TRG01': 'Terengganu',
-      'PHG01': 'Pahang',
-      'NSN01': 'Negeri Sembilan',
+
+      // Perak
+      'PRK01': 'Ipoh',
+      'PRK02': 'Taiping',
+      'PRK03': 'Teluk Intan',
+      'PRK04': 'Kuala Kangsar',
+      'PRK05': 'Seberang Perai',
+      'PRK06': 'Selama',
+      'PRK07': 'Bagan Datuk',
+
+      // Kedah
+      'KDH01': 'Alor Setar',
+      'KDH02': 'Sungai Petani',
+      'KDH03': 'Kuala Muda',
+      'KDH04': 'Kubang Pasu',
+      'KDH05': 'Pokok Sena',
+      'KDH06': 'Padang Terap',
+      'KDH07': 'Langkawi',
+
+      // Kelantan
+      'KTN01': 'Kota Bharu',
+      'KTN02': 'Tumpat',
+      'KTN03': 'Pasir Mas',
+
+      // Terengganu
+      'TRG01': 'Kuala Terengganu',
+      'TRG02': 'Besut',
+      'TRG03': 'Setiu',
+      'TRG04': 'Marang',
+
+      // Pahang
+      'PHG01': 'Kuantan',
+      'PHG02': 'Raub',
+      'PHG03': 'Jerantut',
+      'PHG04': 'Temerloh',
+      'PHG05': 'Rompin',
+      'PHG06': 'Maran',
+
+      // Negeri Sembilan
+      'NSN01': 'Seremban',
+      'NSN02': 'Port Dickson',
+      'NSN03': 'Jempol',
+
+      // Melaka
       'MLK01': 'Melaka',
-      'SBH01': 'Sabah',
-      'SWK01': 'Sarawak',
+
+      // Sabah
+      'SBH01': 'Kota Kinabalu',
+      'SBH02': 'Beaufort',
+      'SBH03': 'Kudat',
+      'SBH04': 'Sandakan',
+      'SBH05': 'Lahad Datu',
+      'SBH06': 'Tawau',
+
+      // Sarawak
+      'SWK01': 'Kuching',
+      'SWK02': 'Samarahan',
+      'SWK03': 'Sri Aman',
+      'SWK04': 'Betong',
+      'SWK05': 'Sarikei',
+      'SWK06': 'Kapit',
+      'SWK07': 'Bintulu',
+      'SWK08': 'Miri',
+      'SWK09': 'Limbang',
     };
 
     return zoneDisplayNames[zoneCode.toUpperCase()] ??
@@ -216,19 +428,62 @@ class PrayerTimesService {
     try {
       // Map city names to zone codes for e-solat.gov.my API
       final cityCoordinates = {
-        'Wilayah Persekutuan': 'WLY01',
-        'Selangor': 'SGR01',
-        'Johor': 'JHR01',
+        'Kuala Lumpur': 'WLY01',
+        'Shah Alam': 'SGR01',
+        'Putrajaya': 'SGR02',
+        'Cyberjaya': 'SGR02',
+        'Petaling Jaya': 'SGR03',
+        'Johor Bahru': 'JHR01',
+        'Batu Pahat': 'JHR02',
+        'Kluang': 'JHR03',
+        'Pontian': 'JHR04',
         'Pulau Pinang': 'PNG01',
-        'Perak': 'PRK01',
-        'Kedah': 'KDH01',
-        'Kelantan': 'KTN01',
-        'Terengganu': 'TRG01',
-        'Pahang': 'PHG01',
-        'Negeri Sembilan': 'NSN01',
+        'Ipoh': 'PRK01',
+        'Taiping': 'PRK02',
+        'Teluk Intan': 'PRK03',
+        'Kuala Kangsar': 'PRK04',
+        'Seberang Perai': 'PRK05',
+        'Selama': 'PRK06',
+        'Bagan Datuk': 'PRK07',
+        'Alor Setar': 'KDH01',
+        'Sungai Petani': 'KDH02',
+        'Kuala Muda': 'KDH03',
+        'Kubang Pasu': 'KDH04',
+        'Pokok Sena': 'KDH05',
+        'Padang Terap': 'KDH06',
+        'Langkawi': 'KDH07',
+        'Kota Bharu': 'KTN01',
+        'Tumpat': 'KTN02',
+        'Pasir Mas': 'KTN03',
+        'Kuala Terengganu': 'TRG01',
+        'Besut': 'TRG02',
+        'Setiu': 'TRG03',
+        'Marang': 'TRG04',
+        'Kuantan': 'PHG01',
+        'Raub': 'PHG02',
+        'Jerantut': 'PHG03',
+        'Temerloh': 'PHG04',
+        'Rompin': 'PHG05',
+        'Maran': 'PHG06',
+        'Seremban': 'NSN01',
+        'Port Dickson': 'NSN02',
+        'Jempol': 'NSN03',
         'Melaka': 'MLK01',
-        'Sabah': 'SBH01',
-        'Sarawak': 'SWK01',
+        'Kota Kinabalu': 'SBH01',
+        'Beaufort': 'SBH02',
+        'Kudat': 'SBH03',
+        'Sandakan': 'SBH04',
+        'Lahad Datu': 'SBH05',
+        'Tawau': 'SBH06',
+        'Kuching': 'SWK01',
+        'Samarahan': 'SWK02',
+        'Sri Aman': 'SWK03',
+        'Betong': 'SWK04',
+        'Sarikei': 'SWK05',
+        'Kapit': 'SWK06',
+        'Bintulu': 'SWK07',
+        'Miri': 'SWK08',
+        'Limbang': 'SWK09',
       };
 
       final zoneCode = cityCoordinates[city];
@@ -392,7 +647,7 @@ class PrayerTimesService {
     return {'name': 'Subuh', 'time': subuhPrayer['time']};
   }
 
-  // Get location name from coordinates (reverse geocoding)
+  // Get location name from coordinates (reverse geocoding) - enhanced for accuracy
   static Future<String> getLocationName(
     double latitude,
     double longitude,
@@ -402,27 +657,75 @@ class PrayerTimesService {
       List<Placemark> placemarks = await placemarkFromCoordinates(
         latitude,
         longitude,
-      ).timeout(const Duration(seconds: 5), onTimeout: () => []);
+      ).timeout(const Duration(seconds: 10), onTimeout: () => []);
 
       if (placemarks.isNotEmpty) {
         final place = placemarks.first;
 
-        // Use the same approach as Kiblat page - simple null-coalescing
-        final locationName =
-            place.locality ??
-            place.subAdministrativeArea ??
-            place.administrativeArea ??
-            place.country ??
-            'Lokasi Semasa';
+        // Try to get the most specific location name possible
+        String? locationName;
 
-        print('Location from geocoding: $locationName');
-        return locationName;
+        // Priority: subLocality (neighborhood/area) > locality (city) > subAdministrativeArea > administrativeArea
+        if (place.subLocality != null && place.subLocality!.isNotEmpty) {
+          locationName = place.subLocality;
+          // If we have subLocality, we might want to combine it with locality
+          if (place.locality != null && place.locality != locationName) {
+            locationName = '$locationName, ${place.locality}';
+          }
+        } else if (place.locality != null && place.locality!.isNotEmpty) {
+          locationName = place.locality;
+        } else if (place.subAdministrativeArea != null &&
+            place.subAdministrativeArea!.isNotEmpty) {
+          locationName = place.subAdministrativeArea;
+        } else if (place.administrativeArea != null &&
+            place.administrativeArea!.isNotEmpty) {
+          locationName = place.administrativeArea;
+        }
+
+        // Filter out generic or unhelpful names
+        if (locationName != null) {
+          final genericNames = [
+            'Malaysia',
+            'Kuala Lumpur',
+            'Wilayah Persekutuan',
+            'Selangor',
+            'Johor',
+            'Pahang',
+            'Perak',
+            'Kedah',
+            'Kelantan',
+            'Terengganu',
+            'Negeri Sembilan',
+            'Melaka',
+            'Sabah',
+            'Sarawak',
+          ];
+
+          if (!genericNames.contains(locationName) && locationName.length > 2) {
+            print(
+              'Location from geocoding: $locationName (lat: $latitude, lng: $longitude)',
+            );
+            return locationName;
+          }
+        }
       }
 
-      return 'Lokasi Semasa';
+      // If geocoding didn't give us a good name, fall back to zone-based name
+      final zoneCode = _getZoneFromCoordinates(latitude, longitude);
+      final zoneLocation = _getLocationNameFromZone(zoneCode);
+      print('Using zone-based location: $zoneLocation (zone: $zoneCode)');
+      return zoneLocation;
     } catch (e) {
       print('Error getting location name: $e');
-      return 'Lokasi Semasa';
+      // Final fallback to zone-based location
+      try {
+        final zoneCode = _getZoneFromCoordinates(latitude, longitude);
+        final zoneLocation = _getLocationNameFromZone(zoneCode);
+        return zoneLocation;
+      } catch (zoneError) {
+        print('Error getting zone location: $zoneError');
+        return 'Lokasi Semasa';
+      }
     }
   }
 }
